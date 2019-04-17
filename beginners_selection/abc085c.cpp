9274a318a81dd7cf9c1dfd54afd4e4bb350c4ec0
Ryota Ikezawa <pavegy@gmail.com>
2019-04-18 02:28:38 +0900

    2019/04/18 - 1

---
+++ beginners_selection/abc085c.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int Amount(int x, int y, int z) {
+    return (10000 * x) + (5000 * y) + (1000 * z);
+}
+
+int main() {
+    int n, yen;
+    std::cin >> n >> yen;
+
+    int x10 = -1, x5 = -1, x1 = -1;
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= n - i; j++) {
+            if (yen == Amount(i, j, (n - i - j))) {
+                x10 = i, x5 = j, x1 = (n - i - j);
+            }
+        }
+    }
+
+    std::cout << x10 << " " << x5 << " " << x1 << std::endl;
+    return 0;
+}
