7d99d46e0fa36a3984eaf781a004496f96b67455
Ryota Ikezawa <pavegy@gmail.com>
2019-04-24 00:47:33 +0900

    AOJ 2019/04/24 - 1

--- AOJ/alds1_1_d.cpp
+++ AOJ/alds1_1_d.cpp
@@ -1,4 +1,23 @@
-//
-// Created by 池澤涼太 on 2019-04-24.
-//
+#include <iostream>
+#include <algorithm>
 
+using namespace std;
+static const int MAX = 200000;
+
+int main() {
+    int n, r[MAX];
+    cin >> n;
+
+    for (int i = 0; i < n; i++) cin >> r[i];
+
+    int maxv = MAX * -10000;
+    int minv = r[0];
+
+    for (int i = 1; i < n; i++) {
+        maxv = max(maxv, r[i] - minv);
+        minv = min(minv, r[i]);
+    }
+
+    cout << maxv << endl;
+    return 0;
+}
