cffc1cb6e19d95812c40c2f68f9513d67c114cb3
Ryota Ikezawa <pavegy@gmail.com>
2019-05-03 02:00:56 +0900

    2019/05/03 - Dictionary

---
+++ AOJ/alds1_4_c.cpp
@@ -0,0 +1,33 @@
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <map>
+
+using namespace std;
+
+#define rep(i, N) for (int i = 0; i < N; ++i)
+
+static const char *INSERT = "insert";
+static const char *FIND = "find";
+map<string, bool> data;
+
+int main() {
+    int n, strMax = 12;
+    scanf("%d", &n);
+    char action[strMax], key[strMax];
+    rep(i, n) {
+        scanf("%s %s", action, key);
+        if (strcmp(INSERT, action) == 0) {
+            data[key] = true;
+        } else if (strcmp(FIND, action) == 0) {
+            if (data[key]) {
+                printf("yes\n");
+            } else {
+                printf("no\n");
+            }
+        }
+    }
+
+    return 0;
+}
