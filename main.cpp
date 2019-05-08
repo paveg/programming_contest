88c588d4d48c8f47d5a2462664231dcb4d8fef1c
Ryota Ikezawa <pavegy@gmail.com>
2019-05-09 03:35:01 +0900

    2019/05/09 - Update snippet

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
-#include <iostream>
-
-#define rep(i, N) for (LL i = 0; i < N; ++i)
-typedef long long int LL;
+#include <cstdio>
 
 using namespace std;
 
+typedef long long int ll;
+#define REP(i, I, N) for (int i = I; i < N; ++i)
+#define rep(i, N) REP(i, 0, N)
+
 int main() {
-    LL N;
-    cin >> N;
     return 0;
 }
