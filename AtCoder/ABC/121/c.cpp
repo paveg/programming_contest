a0326484d152632796e8052292cdd5cfeca4e1f1
Ryota Ikezawa <pavegy@gmail.com>
2019-05-12 22:51:15 +0900

    2019/05/12 - ABC 121 c use pair

--- AtCoder/ABC/121/c.cpp
+++ AtCoder/ABC/121/c.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <map>
 #include <algorithm>
 
 using namespace std;
@@ -8,33 +9,29 @@ typedef long long int ll;
 #define REP(i, I, N) for (ll i = I; i < N; ++i)
 #define rep(i, N) REP(i, 0, N)
 
-struct shop {
-    ll price;
-    ll stock;
-
-    bool operator<(const shop &another) const {
-        return price < another.price;
-    }
-};
-
 int main() {
     ll N, M, ans = 0;
     cin >> N >> M;
-    vector<shop> S(N);
-    ll A, B;
-    rep(i, N) {
-        cin >> A >> B;
-        S[i].price = A;
-        S[i].stock = B;
-    }
-    sort(S.begin(), S.end());
+    vector<pair<ll, ll>> pairs(N);
 
-    rep(i, S.size()) {
-        if (M > S[i].stock) {
-            M -= S[i].stock;
-            ans += S[i].price * S[i].stock;
+    rep(i, N) {
+        ll A, B;
+        cin >> A >> B;
+        pairs[i] = make_pair(A, B); // pairを挿入 price, stock
+    }
+
+    sort(pairs.begin(), pairs.end());
+
+    rep(i, N) {
+        ll price, stocks;
+        price = pairs[i].first;
+        stocks = pairs[i].second;
+
+        if (pairs[i].second < M) {
+            M -= stocks;
+            ans += price * stocks;
         } else {
-            ans += M * S[i].price;
+            ans += M * price;
             cout << ans << "\n";
             return 0;
         }
