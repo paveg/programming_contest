04e6683a1e86222d3ee1f6b2fd132fe0b00c41da
Ryota Ikezawa <pavegy@gmail.com>
2019-05-26 22:56:54 +0900

    add a

--- AtCoder/ABC/128/a.cpp
+++ AtCoder/ABC/128/a.cpp
@@ -1,4 +1,18 @@
-//
-// Created by 池澤涼太 on 2019-05-26.
-//
+#include <iostream>
 
+using namespace std;
+
+typedef long long int ll;
+#define REP(i, I, N) for (ll i = I; i < N; ++i)
+#define rep(i, N) REP(i, 0, N)
+
+int main() {
+    int A, P;
+    cin >> A >> P;
+    int ans = 0;
+    P += A * 3;
+    ans = P / 2;
+
+    cout << ans << endl;
+    return 0;
+}
