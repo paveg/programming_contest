b0613bb447333f86dc40960470d000e122edfbd0
Ryota Ikezawa <pavegy@gmail.com>
2019-06-23 00:14:44 +0900

    main

---
+++ bits/stdc++.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <numeric>
+#include <limits>
+#include <bitset>
+#include <queue>
+#include <stack>
+
--- main.cpp
+++ main.cpp
@@ -1,15 +1,25 @@
-#include <iostream>
-#include <vector>
+#include "bits/stdc++.h"
 
 using namespace std;
 typedef long long ll;
 const ll inf = (1 << 30) - 1;
 const ll infll = (1LL << 61) - 1;
 const ll mod = 1e9 + 7;
+#define REP(i, o, n) for (ll i = o; i < n; i++)
+#define rep(i, n) REP(i, 0, n)
 
 ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+
 ll lcm(ll a, ll b) { return a / gcd(b, a % b) * b; }
 
+void actual() {
+}
+
+
 int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    actual();
     return 0;
 }
