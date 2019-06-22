3c23d0196f09daeb39e7750e5e62a605107eb59d
Ryota Ikezawa <pavegy@gmail.com>
2019-06-22 23:53:32 +0900

    main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+typedef long long ll;
+const ll inf = (1 << 30) - 1;
+const ll infll = (1LL << 61) - 1;
+const ll mod = 1e9 + 7;
 
-typedef long long int ll;
-#define REP(i, I, N) for (int i = I; i < N; ++i)
-#define rep(i, N) REP(i, 0, N)
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return a / gcd(b, a % b) * b; }
 
 int main() {
     return 0;
