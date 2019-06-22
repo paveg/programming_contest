608a7fa56ec867e60ac7f3025e46e217f531d301
Ryota Ikezawa <pavegy@gmail.com>
2019-06-22 23:28:33 +0900

    modify d

--- AtCoder/ABC/131/d.cpp
+++ AtCoder/ABC/131/d.cpp
@@ -8,26 +8,34 @@ const ll inf = (1 << 30) - 1;
 const ll infll = (1LL << 61) - 1;
 const ll mod = 1e9 + 7;
 
-int main() {
-    ll n;
-    cin >> n;
-    vector<ll> a(n), b(n), sum(n);
-    for (int i = 0; i < n; i++) cin >> a[i] >> b[i];
-    sort(a.begin(), a.end(), less<>());
-    sort(b.begin(), b.end(), less<>());
-
-    ll s = 0;
-    for (int i = 0; i < a.size(); i++) {
-        s += a[i];
-        sum[i] = s;
+bool compareByBack(pair<ll, ll> a, pair<ll, ll> b) {
+    if (a.second != b.second) {
+        return a.second < b.second;
+    } else {
+        return a.first < b.first;
     }
+}
+
+int main() {
+    ll n, a, b;
+    cin >> n;
+    vector<pair<ll, ll>> works(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a >> b;
+        works[i] = make_pair(a, b);
+    }
+
+    sort(works.begin(), works.end(), compareByBack);
     auto ans = "Yes";
-    for (int i = 0; i < b.size(); i++) {
-        if (sum[i] > b[i]) {
+    ll sum = 0;
+    for (int i = 0; i < works.size(); i++) {
+        sum += works[i].first;
+        if (sum > works[i].second) {
             ans = "No";
             break;
         }
     }
+
     cout << ans << endl;
     return 0;
 }
